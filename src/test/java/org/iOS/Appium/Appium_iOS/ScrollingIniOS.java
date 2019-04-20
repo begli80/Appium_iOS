b28dc41a276ad21762418e9286a3f25e7b1bea32
Begli <begli80@gmail.com>
2019-04-20 13:37:59 -0500

    Steppers added

--- src/test/java/org/iOS/Appium/Appium_iOS/ScrollingIniOS.java
+++ src/test/java/org/iOS/Appium/Appium_iOS/ScrollingIniOS.java
@@ -3,6 +3,7 @@ package org.iOS.Appium.Appium_iOS;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -26,11 +27,25 @@ public class ScrollingIniOS {
 		IOSDriver<IOSElement>driver=new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"),decap);
 		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		String elementID = driver.findElementByAccessibilityId("Toolbars").getId();
+		String elementID = driver.findElementByAccessibilityId("Steppers").getId();
 		HashMap<String, String> scrollObject = new HashMap<String, String>();
 		scrollObject.put("element", elementID);                                  // for scrolling to a certain element
 		scrollObject.put("toVisible", "not an empty string");
 		js.executeScript("mobile:scroll", scrollObject);
+		
+		
+		// Click on Steppers and do some operations
+		
+		driver.findElementByAccessibilityId("Steppers").click();
+		List<IOSElement> increment=driver.findElementsByName("Increment");
+		increment.get(0).click();
+		increment.get(0).click();
+		System.out.println("Default: "+driver.findElementsByClassName("XCUIElementTypeStaticText").get(0).getText());
+		increment.get(1).click();
+		System.out.println("Tinted: "+driver.findElementsByClassName("XCUIElementTypeStaticText").get(1).getText());
+		increment.get(2).click();
+		increment.get(2).click();
+		System.out.println("Custom: "+driver.findElementsByClassName("XCUIElementTypeStaticText").get(2).getText());
 		
 //		JavascriptExecutor js = (JavascriptExecutor) driver;
 //        HashMap<String, String>scrollObject = new HashMap<>();
